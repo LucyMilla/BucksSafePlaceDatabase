8b9d6631758ab56bf5c8e851f1501acf239e4fd1
Lucy Miller <Lucy.Miller@gmail.com>
2017-12-10 19:20:38 +0000

    Database BusinessAddress Space Error removed

--- BucksSPDb3/Location.sql
+++ BucksSPDb3/Location.sql
@@ -2,7 +2,7 @@
 (
 	[LocationID] INT IDENTITY (1, 1) NOT NULL, 
     [BusinessName] NVARCHAR(20) NULL, 
-    [ BusinessAddress] NVARCHAR(50) NULL,
+    [BusinessAddress] NVARCHAR(50) NULL,
 	[Postcode] NVARCHAR(7) NULL,
 	[TelephoneNumber] INT NULL,
 	PRIMARY KEY CLUSTERED ([LocationID] ASC)
