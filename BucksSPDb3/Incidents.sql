b1a1bcb10d139b84bbce69dfdb01bb947739cc64
Lucy Miller <Lucy.Miller@gmail.com>
2018-01-01 20:59:35 +0000

    Edit to Tag Element from 10 to 25

--- BucksSPDb3/Incidents.sql
+++ BucksSPDb3/Incidents.sql
@@ -4,7 +4,7 @@
     [IncidentTitle] NVARCHAR(25) NULL,
 	[IncidentLocation] NVARCHAR (25) NULL,
 	[IncidentDate] DATETIME NULL, 
-	[Tag] NVARCHAR(10) NULL,
+	[Tag] NVARCHAR(25) NULL,
 	[Report] TEXT NULL, 
     PRIMARY KEY CLUSTERED ([IncidentID] ASC)
 
